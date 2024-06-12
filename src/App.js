6faca2b7a63f742d9bf5f078d90d06a2decd1e6a
Anton Knysh <anton@profairgames.com>
2024-06-12 19:55:29 +0300

    create sneakers-online app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="about">
+          <img src="source/logo.svg" />
+          <div className="aboutInfo">
+            <h3>REACT SNEAKERS</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="utils">
+          <li className="utils-item">
+            <img width={18} height={18} src="source/icons/cart.svg" />
+            <span>1205 $</span>
+          </li>
+          <li className="utils-item">
+            <img width={18} height={18} src="source/icons/profile.svg" />
+            <span>Профиль</span>
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1 className="title">All sneakers</h1>
+        <div className="sneakers">
+          <div className="sneakers-item">
+            <img src="source/sneakers/item-1.jpg" />
+            <div>
+              <p>Male sneakers</p>
+              <p>Nike Blazer Mid Suede</p>
+            </div>
+            <div>
+              <p>Price</p>
+              <p>270$</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
