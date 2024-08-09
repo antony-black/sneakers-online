3a47ae10e659af29cbeb963cf42654fedc0671c7
Anton Knysh <anton@profairgames.com>
2024-08-09 12:35:03 +0300

    cleaned all

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,43 +2,32 @@ import useGlobalState from "../../hooks/useGlobalState";
 import styles from "./Card.module.scss";
 
 export default function Card({ item }) {
-  const { handleCart, isAdded, isFavoriteAdded, handleFavorites } =
-    useGlobalState();
-
   return (
     <div className={styles.sneakersItem}>
       <div className={styles.favorite}>
         <img
           className={styles.heart}
-          onClick={() => handleFavorites(item)}
-          src={
-            !isFavoriteAdded[item.id]
-              ? "../source/icons/heart.svg"
-              : "../source/icons/heart-active.svg"
-          }
+          src="../source/icons/heart.svg"
           alt="heart"
         />
       </div>
-      <img className={styles.item} src={item.image} alt="sneakers" />
+      <img
+        className={styles.item}
+        src={"../../../source/sneakers/item-1.jpg"}
+        alt="sneakers"
+      />
       <div className="sneakers-item-info">
         <a>
-          <p>{item.title}</p>
+          <p>title</p>
         </a>
       </div>
       <div className={styles.sneakersItemPriceInfo}>
         <div className={styles.sneakersItemPriceDetails}>
           <p className={styles.sneakersItemPriceTitle}>Price:</p>
-          <p className={styles.sneakersItemPrice}>{`${item.price}$`}</p>
+          <p className={styles.sneakersItemPrice}>price</p>
         </div>
         <button className="plus" onClick={() => handleCart(item)}>
-          <img
-            src={
-              !isAdded[item.id]
-                ? "source/icons/plus.svg"
-                : "source/icons/check.svg"
-            }
-            alt="plus-button"
-          />
+          <img src="source/icons/plus.svg" alt="plus-button" />
         </button>
       </div>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,35 +3,30 @@ import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
-  const { setCartOpen, total, favorites } = useGlobalState();
   return (
     <nav>
-      <NavLink to={"/"} className={styles.navLink}>
-        <div className={styles.about}>
-          <img src="source/logo.svg" alt="logo" />
-          <div className={styles.aboutInfo}>
-            <h3>REACT SNEAKERS</h3>
-            <p>The Best Sneakers Store</p>
-          </div>
+      <div className={styles.about}>
+        <img src="source/logo.svg" alt="logo" />
+        <div className={styles.aboutInfo}>
+          <h3>REACT SNEAKERS</h3>
+          <p>The Best Sneakers Store</p>
         </div>
-      </NavLink>
+      </div>
       <ul className={styles.utils}>
         <li className={styles.utilsItem} onClick={() => setCartOpen(true)}>
           <img src="source/icons/cart.svg" alt="cart" />
           <span>{total} $</span>
         </li>
         <li className={styles.utilsItem}>
-          <NavLink to={"/favorites"} className={styles.navLink}>
-            <img
-              src={
-                favorites.length > 0
-                  ? "source/icons/heart-active.svg"
-                  : "source/icons/heart.svg"
-              }
-              alt="favorites"
-            />
-            Favorites
-          </NavLink>
+          <img
+            src={
+              favorites.length > 0
+                ? "source/icons/heart-active.svg"
+                : "source/icons/heart.svg"
+            }
+            alt="favorites"
+          />
+          Favorites
         </li>
         <li className={styles.utilsItem}>
           <img src="source/icons/profile.svg" alt="profile" />
