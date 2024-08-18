da18c4795bba05e424a2d2d237259ff7edaf8095
Anton Knysh <anton@profairgames.com>
2024-08-18 07:31:25 +0300

    the title on the Favorites page is visible only there is one item/scss changed

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -14,17 +14,19 @@ export default function Favorites() {
 
   return (
     <div className={styles.favoritesContainer}>
-      <div className={styles.turnBackContainer}>
-        <NavLink to={"/"}>
-          <button className={styles.turnBack}>
-            <img
-              src="../../../source/icons/arrow-back-page.svg"
-              alt="turn-back"
-            />
-          </button>
-        </NavLink>
-        <h2 className={styles.turnBackTitle}>Favorites</h2>
-      </div>
+      {favorites?.length > 0 && (
+        <div className={styles.turnBackContainer}>
+          <NavLink to={"/"}>
+            <button className={styles.turnBack}>
+              <img
+                src="../../../source/icons/arrow-back-page.svg"
+                alt="turn-back"
+              />
+            </button>
+          </NavLink>
+          <h2 className={styles.turnBackTitle}>Favorites</h2>
+        </div>
+      )}
       <div className={styles.favorites}>
         {favorites?.length > 0 ? (
           favorites.map((favItem) => (
