ff9991edb1eb75578189dfc924cb5c1e28c639d6
Anton Knysh <anton@profairgames.com>
2024-08-31 13:06:34 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalState from "./context";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  // <BrowserRouter basename="/sneakers-online">
+  <HashRouter>
     <React.StrictMode>
       <GlobalState>
         <App />
       </GlobalState>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
