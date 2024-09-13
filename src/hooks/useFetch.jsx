53ccf17c7bbc9c78d55efe581a12875a517b5a25
Anton Knysh <anton.knysh@andevart.com>
2024-09-13 19:53:51 +0300

    rework the useFetch()/totalPageNumber

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 import { FetchService } from "../services/FetchService";
+import { PaginationService } from "../services/PaginationService";
 
 export default function useFetch(url, options = {}) {
   const [data, setData] = useState(null);
   const [pending, setPending] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+  const [totalPageNumber, setTotalPageNumber] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       setPending(true);
       try {
-        const response = await FetchService.getData(url, options);
+        const response = await FetchService.getPaginationData(url, options);
         setData(response.data);
+        // TODO: separate from useFetch() (I guess, it'd be better to separate the pagination data)
+        const pageNumber = await PaginationService.getPageCount(url, options.limit);
+        setTotalPageNumber(pageNumber);
+
         setErrorMsg(null);
       } catch (error) {
         setErrorMsg(error.message || "An error occurred");
@@ -23,5 +29,10 @@ export default function useFetch(url, options = {}) {
     fetchData();
   }, [url]);
 
-  return { data, pending, errorMsg };
+  return { 
+    data, 
+    totalPageNumber, 
+    pending, 
+    errorMsg 
+  };
 }
