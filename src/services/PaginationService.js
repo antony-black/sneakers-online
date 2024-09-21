e530b2d17f70ad1cc39f44c7b233d5ca8b78b9b5
Anton Knysh <anton.knysh@andevart.com>
2024-09-21 13:08:23 +0300

    refactored getPageNumbers

--- src/services/PaginationService.js
+++ src/services/PaginationService.js
@@ -1,12 +1,22 @@
-// TODO: try&catch
 export const PaginationService = {
   getPageNumbers(totalPageNumber) {
     // console.log("getPageNumbers >>>>");
-    const pages = [];
-    for (let i = 0; i < totalPageNumber; i++) {
-      pages.push(i + 1);
-    }
+    try {
+      const pages = [];
 
-    return pages;
+      if (typeof totalPageNumber !== "number" || totalPageNumber < 0) {
+        throw new Error(
+          "Invalid totalPageNumber. It must be a non-negative number."
+        );
+      }
+
+      for (let i = 0; i < totalPageNumber; i++) {
+        pages.push(i + 1);
+      }
+
+      return pages;
+    } catch (error) {
+      console.error("Error in getPageNumbers:", error.message);
+    }
   },
 };
